2c10210e6ef121433cdd8323aa9a758561370788
Igor0006 <igordubrovskij69@gmail.com>
2025-09-15 21:35:01 +0300

    fix handshaker error

--- src/main/java/com/cchat/cclient/CclientApplication.java
+++ src/main/java/com/cchat/cclient/CclientApplication.java
@@ -3,8 +3,9 @@ package com.cchat.cclient;
 import java.lang.reflect.Type;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
-import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
@@ -16,24 +17,33 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 public class CclientApplication {
     public static void main(String[] args) throws Exception {
-        final String WS_URL = "ws://localhost:8080/gs-guide-websocket";
+        final String WS_URL = "ws://localhost:8081/gs-guide-websocket";
         final String DEST ="/topic/messages";
 
         WebSocketStompClient stomp = new WebSocketStompClient(new StandardWebSocketClient());
-        stomp.setMessageConverter(new StringMessageConverter());
+        stomp.setMessageConverter(new MappingJackson2MessageConverter());
         stomp.setDefaultHeartbeat(new long[]{10_000, 10_000});
         stomp.setTaskScheduler(new org.springframework.scheduling.concurrent.ConcurrentTaskScheduler());
 
         WebSocketHttpHeaders httpHeaders = new WebSocketHttpHeaders();
+
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYm9iYSIsImlhdCI6MTc1Nzk2MDMzNCwiZXhwIjoxNzU3OTY3NTM0fQ.X9RFUAhJG89VnCzTlXVnT90wFxYLRsg5iT98ZSv9whU";
+
+        if (jwt != null && !jwt.isBlank()) {
+            httpHeaders.add("Authorization", "Bearer " + jwt);
+        }
+        httpHeaders.add("Origin", "http://localhost:8081");
+        httpHeaders.add("Sec-WebSocket-Protocol", "v12.stomp");
 
         System.out.println("Connecting to " + WS_URL + " ...");
         stomp.connect(WS_URL, httpHeaders, new StompSessionHandlerAdapter() {
             @Override public void afterConnected(StompSession s, StompHeaders ch) {
                 System.out.println("Connected. Subscribing to " + DEST);
                 s.subscribe(DEST, new StompFrameHandler() {
-                    @Override public Type getPayloadType(StompHeaders headers) { return String.class; }
+                    @Override public Type getPayloadType(StompHeaders headers) { return Map.class; }
                     @Override public void handleFrame(StompHeaders headers, Object payload) {
-                        System.out.printf("[%s] %s%n", Instant.now(), payload);
+                        @SuppressWarnings("unchecked") Map<String, Object> msg = (Map<String, Object>) payload;
+                        System.out.printf("[%s] %s%n", Instant.now(), msg);
                     }
                 });
             }
