bab55b9e4b050b16365bf9b9a2a67339b26aeedb
Igor0006 <igordubrovskij69@gmail.com>
2025-09-15 20:36:18 +0300

    init commit

---
+++ src/main/java/com/cchat/cclient/CclientApplication.java
@@ -0,0 +1,13 @@
+package com.cchat.cclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cchat/cclient/CclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cchat.cclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
